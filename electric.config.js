cef723fc036b154d07f1387a52760bda412e3294
8llouch <w.martinache@gmail.com>
2018-09-03 18:34:42 +0200

    Update webda site

--- electric.config.js
+++ electric.config.js
@@ -1,22 +1,23 @@
 'use strict';
+
 var marble = require('marble');
 
 module.exports = {
-  metalComponents: ['electric-marble-components'],
-  sassOptions: {
-    includePaths: ['node_modules', marble.src]
-  },
-  envOptions: {
-    website: {
-      basePath: '/'
-    },
-    ghpages: {
-      basePath: '/webda',
-      deployOptions: {
-        branch: 'gh-pages',
-        repo: 'https://' + process.env['GH_TOKEN'] + '@github.com/loopingz/webda.git'
-      }
-    }
-  },
-  vendorSrc: ['node_modules/marble/build/fonts/**']
+	metalComponents: ['electric-marble-components', 'marble-topbar'],
+	sassOptions: {
+		includePaths: ['node_modules', marble.src]
+	},
+	envOptions: {
+		website: {
+		  basePath: '/'
+		},
+		ghpages: {
+		  basePath: '/webda',
+		  deployOptions: {
+			branch: 'gh-pages',
+			repo: 'https://' + process.env['GH_TOKEN'] + '@github.com/loopingz/webda.git'
+		  }
+		}
+	  },
+	vendorSrc: ['node_modules/marble/build/fonts/**']
 };
