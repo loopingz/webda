96a95f21c8cc3cdb24e24821fe92268569e40a06
Remi Cattiau <remi@cattiau.com>
2019-03-24 08:53:10 -0700

    Update webda version

--- test/webda.test.js
+++ test/webda.test.js
@@ -65,7 +65,7 @@ describe("Webda", function() {
   });
   describe("getVersion()", function() {
     it("current", function() {
-      assert.equal(webda.getVersion(), "0.10.7");
+      assert.equal(webda.getVersion(), "0.10.8");
     });
   });
   describe("utils", function() {
